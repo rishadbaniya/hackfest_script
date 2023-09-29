6e0f5eecfe467718eb85654845e11cb0ef65ef3e
rishadbaniya <rbdroid01@gmail.com>
2023-09-29 22:34:29 +0545

    Removed unwanted

--- script_2.js
+++ script_2.js
@@ -34,35 +34,34 @@ const timestamp = require('unix-timestamp');
 // Important : The month field of the Date starts from 0
 // Any commit before HACKATHON_START_DATE and after HACKATHON_END_DATE would be considered disqualified
 // Unix Epoch time of starting Hackathon Date and Ending Hackathon Date
-
 let HACKATHON_START_DATE = new Date(2022, 7, 5, 12, 0, 0).getTime() / 1000;
-let HACKATHON_END_DATE = new Date(2022, 7, 7, 9,0,0).getTime() / 1000;
+let HACKATHON_END_DATE = new Date(2022, 7, 7, 9, 0, 0).getTime() / 1000;
 
 const CSV_FIELDS = {
-    FIRST_NAME : "First Name",
-    LAST_NAME : "Last Name",
-    TSHIRT_SIZE : "T-shirt Size",
-    GENDER : "Gender",
-    COLLEGE : "College",
-    EMAIL : "Email",
-    TEAM_NAME : "Team Name",
-    DEVFOLIO_PROFILE : "Devfolio Profile",
-    SUBMISSION_URL : "Submission URL",
-    GITHUB_URL : "GithubURL",
-    TRACKS : "Tracks",
-    WINNERS : "Winners",
-    CUSTOM_QUESTIONS : "Custom Questions",
-    QUALIFIED_OR_NOT : "Qualified Or Not" // It's a custom field
+    FIRST_NAME: "First Name",
+    LAST_NAME: "Last Name",
+    TSHIRT_SIZE: "T-shirt Size",
+    GENDER: "Gender",
+    COLLEGE: "College",
+    EMAIL: "Email",
+    TEAM_NAME: "Team Name",
+    DEVFOLIO_PROFILE: "Devfolio Profile",
+    SUBMISSION_URL: "Submission URL",
+    GITHUB_URL: "GithubURL",
+    TRACKS: "Tracks",
+    WINNERS: "Winners",
+    CUSTOM_QUESTIONS: "Custom Questions",
+    QUALIFIED_OR_NOT: "Qualified Or Not" // It's a custom field
 };
 
 let PARAMS = {
-     maxBuffer : 10000*10000, // Max Buffer size for stdout
-     timeout : 480 * 1000, // Max timeout of 8 minutes per repo
-     stdio : [0, 'ignore','ignore'],
-     shell : "sh"
+    maxBuffer: 10000 * 10000, // Max Buffer size for stdout
+    timeout: 480 * 1000, // Max timeout of 8 minutes per repo
+    stdio: [0, 'ignore', 'ignore'],
+    shell: "sh"
 };
 
-const INPUT_CSV_DATA = fs.readFileSync(__dirname + "/output/github_url_extracted.csv", { encoding:'utf8', flag:'r'}); 
+const INPUT_CSV_DATA = fs.readFileSync(__dirname + "/output/github_url_extracted.csv", {encoding: 'utf8', flag: 'r'});
 const delimiter = ',';
 
 // Qualified has 3 values :
@@ -72,12 +71,12 @@ const delimiter = ',';
 let GLOBAL_JSON = csv_lib.csvToJson(INPUT_CSV_DATA, delimiter);
 let DOUBLE_QUOTE_REMOVED_FROM_HEADING_JSON = [];
 
-GLOBAL_JSON.forEach((d)=>{
+GLOBAL_JSON.forEach((d) => {
     let newObj = {};
-    for(let i in d){
-      let oldKey = i;
-      let newKey = i.replaceAll('"', "");
-      newObj[newKey] = d[oldKey];
+    for (let i in d) {
+        let oldKey = i;
+        let newKey = i.replaceAll('"', "");
+        newObj[newKey] = d[oldKey];
     }
     DOUBLE_QUOTE_REMOVED_FROM_HEADING_JSON.push(newObj);
 });
@@ -90,58 +89,58 @@ let NO_OF_RESOLVED = 0;
 
 GLOBAL_JSON.forEach((data, i) => {
     if (data["Qualified Or Not"] === "Qualified" || data["Qualified Or Not"] === "Disqualified") {
-    }else{
+    } else {
         GLOBAL_JSON[i]["Qualified Or Not"] = "Unresolved";
     }
 
-  if(data['GithubURL'] === 'Not Submitted' || data[CSV_FIELDS.TEAM_NAME] === "N/A"){
-    data['Qualified Or Not'] = "Disqualified";
-  }
+    if (data['GithubURL'] === 'Not Submitted' || data[CSV_FIELDS.TEAM_NAME] === "N/A") {
+        data['Qualified Or Not'] = "Disqualified";
+    }
 });
 
 // Delete the directory AxBxCxDxExFxGxYx if it's there because it's going to create conflicts
-try{ childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx", {stdin : [0, "ignore", "ignore"], shell : "sh"})}catch(_){  }
+try {childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx", {stdin: [0, "ignore", "ignore"], shell: "sh"})} catch (_) {}
 
 const checkAllData = () => {
     GLOBAL_JSON.forEach((data, index) => {
         if (data["Qualified Or Not"] == "Unresolved") {
             console.log(`Checking ${index + 1} of ${NO_OF_TEAMS}......`)
-        try {
-            // Naming the repository something complex, so that it doesn't cause 
-            // name conflict and cause issues while cloning
-            let command = `git clone ${data[CSV_FIELDS.GITHUB_URL]} AxBxCxDxExFxGxYx`;
-            console.log(data[CSV_FIELDS.GITHUB_URL]);
-            childProcess.execSync(command, PARAMS);
+            try {
+                // Naming the repository something complex, so that it doesn't cause 
+                // name conflict and cause issues while cloning
+                let command = `git clone ${data[CSV_FIELDS.GITHUB_URL]} AxBxCxDxExFxGxYx`;
+                console.log(data[CSV_FIELDS.GITHUB_URL]);
+                childProcess.execSync(command, PARAMS);
 
-            // Reverse the log from the oldest and in YYYY-MM-DD format
-            let commit_history = childProcess.execSync('git log --reverse --format="%at"', {encoding : "UTF-8", cwd : "./AxBxCxDxExFxGxYx", stdin : [0, 'ignore', 'ignore'], shell : "sh"});
-            // TODO : Handle issues when the Repository is empty 
-            // Only gets the first commit of the reverse i.e the oldest commit
-            const OLDEST_COMMIT_DATE = parseInt(commit_history.slice(0,10));
+                // Reverse the log from the oldest and in YYYY-MM-DD format
+                let commit_history = childProcess.execSync('git log --reverse --format="%at"', {encoding: "UTF-8", cwd: "./AxBxCxDxExFxGxYx", stdin: [0, 'ignore', 'ignore'], shell: "sh"});
+                // TODO : Handle issues when the Repository is empty 
+                // Only gets the first commit of the reverse i.e the oldest commit
+                const OLDEST_COMMIT_DATE = parseInt(commit_history.slice(0, 10));
 
-            console.log(OLDEST_COMMIT_DATE);
+                console.log(OLDEST_COMMIT_DATE);
 
-            if (OLDEST_COMMIT_DATE <= HACKATHON_START_DATE || OLDEST_COMMIT_DATE >= HACKATHON_END_DATE){
-                console.log("NOT QUALIFIED");
-                GLOBAL_JSON[index]["Qualified Or Not"] = "Disqualified";
-            }else{
-                console.log("QUALIFIED");
-                GLOBAL_JSON[index]["Qualified Or Not"] = "Qualified";
+                if (OLDEST_COMMIT_DATE <= HACKATHON_START_DATE || OLDEST_COMMIT_DATE >= HACKATHON_END_DATE) {
+                    console.log("NOT QUALIFIED");
+                    GLOBAL_JSON[index]["Qualified Or Not"] = "Disqualified";
+                } else {
+                    console.log("QUALIFIED");
+                    GLOBAL_JSON[index]["Qualified Or Not"] = "Qualified";
+                }
+
+                // TODO : Make sure yes | rm -r AxBxCxDxExFxGxYx works on Windows default shell for execSync as well, coz this was only tested for "sh" shell, 
+                // which is the default shell for execSync in linux
+                //
+                // Remove the local repository created and make space for another repo
+                try {childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx/", {stdin: [0, "ignore", "ignore"], shell: "sh"})} catch (_) {}
+                NO_OF_RESOLVED += 1;
+
+            } catch (e) {
+                // Delete the directory AxBxCxDxExFxGxYx if it's there because it's going to create conflicts
+                try {childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx/", {stdin: [0, 'ignore', 'ignore'], shell: "sh"})} catch (_) {}
             }
-            
-            // TODO : Make sure yes | rm -r AxBxCxDxExFxGxYx works on Windows default shell for execSync as well, coz this was only tested for "sh" shell, 
-            // which is the default shell for execSync in linux
-            //
-            // Remove the local repository created and make space for another repo
-            try{ childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx/", {stdin : [0, "ignore", "ignore"], shell : "sh"})}catch(_){  }
-            NO_OF_RESOLVED += 1;
-    
-        } catch (e) {
-            // Delete the directory AxBxCxDxExFxGxYx if it's there because it's going to create conflicts
-            try{ childProcess.execSync("yes | rm -r AxBxCxDxExFxGxYx/", {stdin : [0, 'ignore', 'ignore'], shell : "sh" })}catch(_){  }
-         }
-     }
-    
+        }
+
     });
 }
 
@@ -160,11 +159,11 @@ const containsAnyUnResolved = () => {
     let NO_OF_UNRESOLVED_TEAMS = 0;
 
     GLOBAL_JSON.forEach((data, index) => {
-        if(data[CSV_FIELDS.QUALIFIED_OR_NOT] == "Qualified"){
+        if (data[CSV_FIELDS.QUALIFIED_OR_NOT] == "Qualified") {
             NO_OF_QUALIFIED_TEAMS++;
-        }else if(data[CSV_FIELDS.QUALIFIED_OR_NOT] == "Disqualified"){
+        } else if (data[CSV_FIELDS.QUALIFIED_OR_NOT] == "Disqualified") {
             NO_OF_DISQUALIFIED_TEAMS++;
-        }else{
+        } else {
             NO_OF_UNRESOLVED_TEAMS++;
         }
     });
@@ -178,10 +177,10 @@ const containsAnyUnResolved = () => {
 
 // Checks until the "Unresolved" fields appear as "Qualified" or "Not Qualified"
 // for upto MAX_ITERATIONS
-while(NO_OF_ITERATIONS <= MAX_ITERATIONS){
+while (NO_OF_ITERATIONS <= MAX_ITERATIONS) {
     console.log(`ITERATION : ${NO_OF_ITERATIONS} of ${MAX_ITERATIONS}`)
     console.log('-------------------------------------------------------------------------------------')
-    if (containsAnyUnResolved()){
+    if (containsAnyUnResolved()) {
         checkAllData();
     }
     NO_OF_ITERATIONS++;
@@ -189,7 +188,7 @@ while(NO_OF_ITERATIONS <= MAX_ITERATIONS){
 
 // Converting all Unresolved to Disqualified
 GLOBAL_JSON = GLOBAL_JSON.map((d) => {
-    if(d[CSV_FIELDS.QUALIFIED_OR_NOT] === "Unresolved"){
+    if (d[CSV_FIELDS.QUALIFIED_OR_NOT] === "Unresolved") {
         d[CSV_FIELDS.QUALIFIED_OR_NOT] = "Disqualified";
     }
     return d;
@@ -203,13 +202,13 @@ let DISQUALIFIED_JSON = GLOBAL_JSON.filter((d) => {
     return d[CSV_FIELDS.QUALIFIED_OR_NOT] === "Disqualified";
 });
 
-if(QUALIFIED_JSON.length != 0){
+if (QUALIFIED_JSON.length != 0) {
     const QUALIFIED_CSV = json_to_csv.parse(QUALIFIED_JSON);
     const QUALIFIED_CSV_PATH = __dirname + "/output/Qualified.csv";
     fs.writeFileSync(QUALIFIED_CSV_PATH, QUALIFIED_CSV);
 }
 
-if(DISQUALIFIED_JSON.length != 0){
+if (DISQUALIFIED_JSON.length != 0) {
     const DISQUALIFIED_CSV = json_to_csv.parse(DISQUALIFIED_JSON);
     const DISQUALIFIED_CSV_PATH = __dirname + "/output/Disqualified.csv";
     fs.writeFileSync(DISQUALIFIED_CSV_PATH, DISQUALIFIED_CSV);
